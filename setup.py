cefb7b89bfbfb3d2da2d50205380013f8ee0ee1e
Olle Vidner <olle@vidner.se>
2016-12-05 18:18:46 +0100

    Bumped version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-rest-framework-expandable',
-    version='0.0.3',
+    version='0.0.4',
     description='Mixins for DRF to allow expansion of fields.',
     long_description='',
     author='Olle Vidner',
