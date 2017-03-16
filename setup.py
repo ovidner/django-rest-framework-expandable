59d205b2c588db36aa73b9b21f9cefa29c8b3b63
Olle Vidner <olle@vidner.se>
2017-03-16 16:34:46 +0100

    Bumped version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='djangorestframework-expandable',
-    version='0.0.5',
+    version='0.0.6',
     description='Mixins for DRF to allow expansion of fields.',
     author='Olle Vidner',
     author_email='olle@vidner.se',
